993b0f81ee902aeaef0671ce44b78cdd85cc60d0
zjeb <szymon.szostek008@gmail.com>
2024-04-23 14:56:26 +0200

    tablice jo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Zadanie_13_2p;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        tablice tablice = new tablice();
+        tablice.powitanie();
+        int[] tab = tablice.Stworz_tablice();
+        Console.Clear();
+        tablice.Wyswietl_Tablice(tab);
+        tablice.StortujBabelkowo(tab);
+        tablice.Wyswietl_Tablice(tab);
+
+    }
+}
---
+++ tablice.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_13_2p
+{
+
+    public class tablice
+    {
+        public int liczba = 20;
+        public void powitanie()
+        {
+            Console.WriteLine("WItaj w pliku tablice!");
+        }
+        public int [] Stworz_tablice()
+        {
+            Console.WriteLine("Ile elementów ma mieć tablica? ");
+            int dl = int.Parse(Console.ReadLine());
+
+            int[] ints = new int[dl];
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                Console.Write("Podaj kolejny element tablicy: ");
+                    ints[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            return ints;
+        }
+        public void Wyswietl_Tablice(int[] tab)
+        {
+            Console.WriteLine("Elementy tablice: ");
+            foreach (int i in tab)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.WriteLine();
+        }
+        public void StortujBabelkowo(int[] tab)
+        {
+            for (int i = 0;i < tab.Length;i++)
+            {
+                for (int j = 1; j < tab.Length;j++)
+                {
+                    if (tab[j-1] > tab[j])
+                    {
+                        int temp = tab[j - 1];
+                        tab[j - 1] = tab[j];
+                        tab[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
